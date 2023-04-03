1d13c62503125ed58db49ba999f4ca8f37e722cd
Christophe van Baalen <christophe@gpsgate.com>
2023-04-03 11:46:31 +0200

    Added error if commenting empty stuff

--- src/components/post/PostComponent.js
+++ src/components/post/PostComponent.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import './PostComponent.css';
-import PostHeaderComponent from './PostHeaderComponent';
-import CommentComponent from '../comment/CommentComponent';
-import { CSSTransition } from 'react-transition-group'
-import Comment from '../../model/Comment';
-import TextareaAutosize from 'react-autosize-textarea/lib';
-import { postRef } from '../../firebase';
+import React, { Component } from "react";
+import "./PostComponent.css";
+import PostHeaderComponent from "./PostHeaderComponent";
+import CommentComponent from "../comment/CommentComponent";
+import { CSSTransition } from "react-transition-group";
+import Comment from "../../model/Comment";
+import TextareaAutosize from "react-autosize-textarea/lib";
+import { postRef } from "../../firebase";
 
 class PostComponent extends Component {
   constructor(props) {
@@ -16,42 +16,56 @@ class PostComponent extends Component {
       footer: this.props.post.footer,
       commentIndex: 0,
       intervalId: 0,
-      newComment: '',
-    }
+      newComment: "",
+      error: "",
+    };
   }
 
   handleCommentChange = (event) => {
-    this.setState({newComment: event.target.value});
-  }
+    this.setState({ newComment: event.target.value, error: "" });
+  };
 
   handleSubmit = () => {
-    const author = this.props.author;
-    const newComment = new Comment(author.name, this.state.newComment);
-    postRef.child(this.props.post.id).child('comments').push().set(newComment);
-    this.setState({newComment: ''});
-  }
+    const { comment } = this.state;
+
+    if (!comment) {
+      this.setState({ ...this.state, error: "Öh, du måste skriva nått" });
+    } else {
+      const author = this.props.author;
+      const newComment = new Comment(author.name, comment);
+      postRef
+        .child(this.props.post.id)
+        .child("comments")
+        .push()
+        .set(newComment);
+      this.setState({ newComment: "", error: "" });
+    }
+  };
 
   render() {
-    const {post} = this.props;
-    const {newComment} = this.state;
+    const { post } = this.props;
+    const { newComment, error } = this.state;
 
     const comments = post.comments ? Object.values(post.comments) : [];
-    
+
     return (
-      <CSSTransition
-        timeout={500}>
+      <CSSTransition timeout={500}>
         <div className="Post">
-          <PostHeaderComponent author={this.props.post.author}/>
+          <PostHeaderComponent author={this.props.post.author} />
           <div className="PostContent">
             Hej Folkets {post.header}-grupp!
-            <br/><br/>
+            <br />
+            <br />
             {post.content}
-            <br/><br/>
+            <br />
+            <br />
             {post.footer} +/-?
           </div>
-          <hr/>
+          <hr />
           <div className="Comments">
-            {comments.map((obj, i) => <CommentComponent comment={obj} key={i}/>)}
+            {comments.map((obj, i) => (
+              <CommentComponent comment={obj} key={i} />
+            ))}
           </div>
           <div className="AddComment">
             <TextareaAutosize
@@ -59,9 +73,10 @@ class PostComponent extends Component {
               type="text"
               value={newComment}
               onChange={this.handleCommentChange}
-              placeholder='Kommentera...'
+              placeholder="Kommentera..."
               rows={1}
             />
+            {error && <div className="error">{error}</div>}
             <button type="submit" onClick={this.handleSubmit}>
               <h4>Skicka</h4>
             </button>

5869eba357e24bfe03a568a1d40f6b2bb5a1b5b0
Christophe van Baalen <christophe@gpsgate.com>
2023-04-03 11:34:22 +0200

    Added error if submitting empty stuff

--- src/components/create_post/CreatePostComponent.js
+++ src/components/create_post/CreatePostComponent.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import './CreatePostComponent.css';
-import TextareaAutosize from 'react-autosize-textarea/lib';
+import React, { Component } from "react";
+import "./CreatePostComponent.css";
+import TextareaAutosize from "react-autosize-textarea/lib";
 
 export default class CreatePostComponent extends Component {
   constructor(props) {
@@ -10,7 +10,8 @@ export default class CreatePostComponent extends Component {
       content: this.props.post.content,
       footer: this.props.post.footer,
       author: this.props.author,
-    }
+      error: "",
+    };
 
     this.handleHeaderChange = this.handleHeaderChange.bind(this);
     this.handleContentChange = this.handleContentChange.bind(this);
@@ -19,63 +20,77 @@ export default class CreatePostComponent extends Component {
   }
 
   handleHeaderChange(event) {
-    this.setState({header: event.target.value});
+    this.setState({ header: event.target.value });
   }
 
   handleContentChange(event) {
-    this.setState({content: event.target.value});
+    this.setState({ content: event.target.value });
   }
 
   handleFooterChange(event) {
-    this.setState({footer: event.target.value});
+    this.setState({ footer: event.target.value });
   }
 
   handleSubmit(event) {
-    this.props.onSubmit({
-      post: {
-        header: this.state.header,
-        content: this.state.content,
-        footer: this.state.footer,
-        author: this.state.author.name,
-        comments: {},
-      }
-    });
-    event.preventDefault();
-    this.setState({
-      header: '',
-      content: '',
-      footer: ''
-    });
+    // TODO Don't post if no data!
+    const { header, content, footer } = this.state;
+    if (!header || !content || !footer) {
+      event.preventDefault();
+      this.setState({
+        ...this.state,
+        error: "Alla fält måste vara ifyllda",
+      });
+    } else {
+      this.props.onSubmit({
+        post: {
+          header: this.state.header,
+          content: this.state.content,
+          footer: this.state.footer,
+          author: this.state.author.name,
+          comments: {},
+        },
+      });
+      event.preventDefault();
+      this.setState({
+        header: "",
+        content: "",
+        footer: "",
+        error: "",
+      });
+    }
   }
 
   render() {
+    const { error } = this.state;
     return (
       <form className="CreatePost" onSubmit={this.handleSubmit}>
         <div className="CreatePostHeader">
-        Hej Folkets {' '}
+          Hej Folkets{" "}
           <input
             type="text"
             value={this.state.header}
-            onChange={this.handleHeaderChange} />
-        -grupp!
+            onChange={this.handleHeaderChange}
+          />
+          -grupp!
         </div>
         <TextareaAutosize
           className="TextareaAutosize"
           rows={3}
           onChange={this.handleContentChange}
           value={this.state.content}
-          placeholder=''
+          placeholder=""
         />
         <div className="CreatePostFooter">
           <input
             type="text"
             value={this.state.footer}
-            onChange={this.handleFooterChange} />
-          {' '} +/-?
+            onChange={this.handleFooterChange}
+          />{" "}
+          +/-?
         </div>
+        {error && <div className="error">{error}</div>}
         <div className="CreatePostSubmit">
-          <button
-            type="submit">
+          <button type="submit">
             <h3>Publicera</h3>
           </button>
         </div>

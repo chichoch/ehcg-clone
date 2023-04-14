5571c91bd0cbda18d3a51f170b7c7d0518414a37
Christophe van Baalen <christophe@gpsgate.com>
2023-04-14 11:41:15 +0200

    Made 'login' look better

--- src/components/CreateAuthorComponent.js
+++ src/components/CreateAuthorComponent.js
@@ -1,24 +1,30 @@
-import React from 'react';
+import React from "react";
+import "./CreateAuthorComponent.css";
 
 export const CreateAuthorComponent = (props) => {
-  const [name, setName] = React.useState('');
+  const [name, setName] = React.useState("");
+  const [error, setError] = React.useState("");
 
-  const onNameChange = event => setName(event.target.value);
+  const onNameChange = (event) => setName(event.target.value);
 
   const onSubmit = () => {
+    if (!name) {
+      setError("Du måste skriva in ett namn");
+      return;
+    }
     props.setAuthor(name);
-  }
+  };
 
   return (
-    <div className="CreatePost">
-      <h1>Skriv in ditt namn:</h1>
-      <input value={name} type="text" onChange={onNameChange} />
-      <button
-        type="submit"
-        onClick={onSubmit}
-      >
-        <h3>LESS GO</h3>
-      </button>
+    <div className="AuthorContainer">
+      <div className="CreateAuthor">
+        <h1>Skriv in ditt namn:</h1>
+        <input value={name} type="text" onChange={onNameChange} />
+        {error && <div className="error">{error}</div>}
+        <button type="submit" onClick={onSubmit}>
+          <h3>LESS GO</h3>
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
